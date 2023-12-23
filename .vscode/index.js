2c5d1df1a5b0199c54c8319d5dd96c9dfcb13b90
marvelousdre <110522817+marvelousdre@users.noreply.github.com>
2023-12-23 10:53:24 -0600

    Added styling to input box and submit button

---
+++ .vscode/index.js
@@ -0,0 +1,8 @@
+let input = document.getElementById("input-box"); 
+let button = document.getElementById("submit-button");
+let showContainer = document.getElementById("show-container"); 
+
+let listContainer = document.querySelector(".list");
+
+let date = new Date();
+console.log(date.getTime());
