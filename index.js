a98393dfe945d2e6a772d4045e6fe43a9829a641
marvelousdre <110522817+marvelousdre@users.noreply.github.com>
2023-12-26 05:41:49 -0600

    Reorganized files and folders

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 let input = document.getElementById("input-box"); 
 let button = document.getElementById("submit-button");
 let showContainer = document.getElementById("show-container"); 
-
 let listContainer = document.querySelector(".list");
 
 let date = new Date();
